b23cf2471b1a5747d4c582a6dc0ca240b388d856
Niko <nikodimstroy@gmail.com>
2025-02-28 00:50:25 +0100

    unit

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+import { getEnvVar } from '../utils/getEnvVar.js';
+
+export const initMongoConnection = async () => {
+  try {
+    const user = getEnvVar('MONGODB_USER');
+    const password = getEnvVar('MONGODB_PASSWORD');
+    const url = getEnvVar('MONGODB_URL');
+    // const db = getEnvVar('MONGODB_DB');
+    const options = getEnvVar('MONGODB_OPTIONS');
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${password}@${url}/?${options}`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.error('Error while setting up mongo connection', error);
+    throw error;
+  }
+};
+
+// mongodb+srv://nikodimstroy:<db_password>@cluster0.cstqs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
---
+++ src/server.js
@@ -0,0 +1,85 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = Number(getEnvVar('PORT', 3000));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'yyyy-mm-dd HH:MM:ss', // Налаштування формату часу
+        },
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.send({
+      status: 200,
+      message: 'GET request for root route successfully accepted!',
+      timestamp: new Date().toISOString(),
+    });
+  });
+
+  app.post('/', (req, res) => {
+    res.send({
+      status: 200,
+      message: 'POST request for root route successfully accepted!',
+      timestamp: new Date().toISOString(),
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contacts in the amount of ${contacts.length} pcs!`,
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id: ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({ message: 'Page Not found' });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
